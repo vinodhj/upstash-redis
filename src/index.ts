0b30e15f1a4c9e7fd11cc6600c7331e49da3b735
Vinodh <vinodh.jeevanantham@gmail.com>
2025-03-17 01:17:13 +0530

    type fix

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export default {
+interface MyExportedHandler extends ExportedHandler<Env> {
+	kafka?(event: any, env: any, ctx: any): Promise<void>;
+  }
+  
+  export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	  return new Response('Hello World!');
 	},
-} satisfies ExportedHandler<Env>;
+  
+	async kafka(event, env, ctx): Promise<void> {
+	  console.log("Received Kafka message:", event.messages);
+	},
+  } satisfies MyExportedHandler;
+  
